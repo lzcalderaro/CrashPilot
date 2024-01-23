39f58e0bb5c3f23d6ee2128d25a2312642cda49c
Luiz Calderaro <luizcalderaro@Luizs-MacBook-Pro.local>
2024-01-23 21:36:52 +0000

    Add classpath

--- crashPilot/build.gradle.kts
+++ crashPilot/build.gradle.kts
@@ -62,7 +62,7 @@ publishing {
         create<MavenPublication>("aar") {
             groupId = "com.github.lzcalderaro"
             artifactId = "crashPilot"
-            version = "0.0.1"
+            version = "0.0.2"
         }
     }
 }
@@ -73,7 +73,7 @@ afterEvaluate {
             register<MavenPublication>("release") {
                 groupId = "com.github.lzcalderaro"
                 artifactId = "crashPilot"
-                version = "0.0.1"
+                version = "0.0.2"
 
                 afterEvaluate {
                     from(components["release"])
