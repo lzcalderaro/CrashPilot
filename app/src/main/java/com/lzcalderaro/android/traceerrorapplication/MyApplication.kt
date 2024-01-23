7e31749f3ebb38d56a9652d6fd964804fa8a64b6
Luiz Calderaro <luizcalderaro@Luizs-MacBook-Pro.local>
2024-01-23 20:36:43 +0000

    Add project

--- app/src/main/java/com/lzcalderaro/android/traceerrorapplication/MyApplication.kt
+++ app/src/main/java/com/lzcalderaro/android/traceerrorapplication/MyApplication.kt
@@ -2,12 +2,12 @@ package com.lzcalderaro.android.traceerrorapplication
 
 import android.app.Application
 import android.content.Context
-import com.lzcalderaro.crashPilot.CrashPilot
+import com.lzcalderaro.crashpilot.CrashPilot
 
 class MyApplication: Application() {
 
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(base)
-        //CrashPilot(this).start().connectToServer("http://192.168.0.163:8080/log").saveLogFile()
+        CrashPilot(this).start().connectToServer("http://192.168.0.163:8080/log").saveLogFile()
     }
 }
